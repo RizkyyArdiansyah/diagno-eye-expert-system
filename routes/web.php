5b06f8860472d08c44835f7e80d08e7f6666bbde
RizkyyArdiansyah <sanssmighty@gmail.com>
2025-05-04 00:30:17 +0700

    Railway deploy

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DiagnosaController;
 
+
+Route::get('/', function () {
+    return response()->json(['status' => 'OK'], 200);
+});
 // AUTH (Register & Login)
 Route::post('/register', [UserController::class, 'register'])->name('register.submit');
 Route::get('/login', [DiagnosaController::class, 'index'])->name('login');
@@ -36,6 +40,8 @@ Route::middleware(['auth', 'cekrole:admin'])->group(function () {
 
 // 👤 Route untuk User biasa
 Route::middleware(['auth', 'cekrole:user'])->group(function () {
-    Route::get('/', [DiagnosaController::class, 'index'])->name('home');
-    Route::post('/', [DiagnosaController::class, 'prosesDiagnosa'])->name('diagnosa.proses');
+    Route::get('/diagnosa', [DiagnosaController::class, 'index'])->name('home');
+    Route::post('/diagnosa', [DiagnosaController::class, 'prosesDiagnosa'])->name('diagnosa.proses');
 });
+
+
