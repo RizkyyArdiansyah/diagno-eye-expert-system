aa3b3cf0fc153c7e1af2c7b71321e960e32eb63c
RizkyyArdiansyah <sanssmighty@gmail.com>
2025-05-03 22:44:15 +0700

    fix: use CommonJS for Vite config for Railway

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-export default {
-    content: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
